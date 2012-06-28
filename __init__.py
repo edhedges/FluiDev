7b2a29e25a7cd3acca94f76cf68e263cb26b55ff
Eddie Hedges <eddiehedges@gmail.com>
2012-06-28 00:16:37 -0500

    first commit (very late) for FluiDev a GUI application that uses PySide

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    FluiDev
+    -------
+
+    FluiDev is a static website builder. It focuses on being simplicity
+    and power.
+
+    Developed by Eddie Hedges
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Eddie Hedges'
